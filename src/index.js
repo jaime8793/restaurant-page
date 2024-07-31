bd31abf5fb84c5343b754a88492568e5e6292ee5
jaime <jamesowiti24@gmail.com>
2024-07-31 15:12:36 +0300

    Server working

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log("Heuy");
+function log() {
+  console.log("Heuy");
+}
+
+log();
+
+for (i = 0; i < 40; i++){
+    log()
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,37 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  mode: "development", // or "production" depending on your environment
+  mode: "development",
   entry: "./src/index.js",
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
+    clean: true,
   },
+  devtool: "inline-source-map",
+  devServer: {
+    static: path.resolve(__dirname, "dist"),
+    open: true,
+    hot: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html", // Path to your HTML template
+    }),
+  ],
 };
