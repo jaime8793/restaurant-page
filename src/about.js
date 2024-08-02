fdd7630f9a8f90e1f07cf992fdc509ef7005dd96
jaime <jamesowiti24@gmail.com>
2024-08-03 00:39:28 +0300

    DONE

--- src/about.js
+++ src/about.js
@@ -2,18 +2,17 @@ function loadAbout() {
   document.getElementById("content").innerHTML = `
         <section class="about">
             <h2>Our Story</h2>
-            <p>[Restaurant Name] is a celebration of African culinary heritage. Founded in [Year], we strive to bring the rich and diverse flavors of Africa to your table. Our chefs are passionate about using traditional recipes and the freshest ingredients to create dishes that honor our roots.</p>
-            <img src="restaurant.jpg" alt="African Restaurant">
+            <p>Odiso Restaurant is a celebration of African culinary heritage. Founded in 2024, we strive to bring the rich and diverse flavors of Africa to your table. Our chefs are passionate about using traditional recipes and the freshest ingredients to create dishes that honor our roots.</p>
         </section>
         <section class="team">
             <h2>Meet Our Team</h2>
             <div class="team-member">
-                <img src="chef.jpg" alt="Chef">
-                <h3>Chef [Name]</h3>
+                <img src="https://utfs.io/f/7faca7a0-754f-41a4-9f50-5a7b1a30f2d2-n92lk7.png" alt="Chef">
+                <h3>Chef Odiso</h3>
                 <p>With over 20 years of experience in African cuisine, Chef [Name] brings a wealth of knowledge and passion to our kitchen.</p>
             </div>
         </section>
     `;
 }
 
-export default loadAbout
+export default loadAbout;
--- src/home.js
+++ src/home.js
@@ -2,7 +2,7 @@
       <div class="sub-slogan">Only the best ingredients used</div>*/
 function loadHome() {
   document.getElementById("content").innerHTML =
-    "<h1>Welcome to [Restaurant Name]</h1><p>Experience the best of African cuisine with us.</p>";
+    `<div class="home-content"><h1>Welcome to Odiso's Restaurant</h1><br/><p>Experience the best of African cuisine with us.</p></div>`;
 }
 //HomePage();
 
