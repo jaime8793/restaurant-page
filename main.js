9ee2643414a475be844e4878df418fdaa82c6b1c
jaime <jamesowiti24@gmail.com>
2024-08-03 00:46:14 +0300

    Initial dist subtree commit

---
+++ main.js
@@ -0,0 +1,14 @@
+/******/ (() => { // webpackBootstrap
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+function log() {
+  console.log("Heuy");
+}
+log();
+for (i = 0; i < 40; i++) {
+  log();
+}
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7O0FBQUEsU0FBU0EsR0FBR0EsQ0FBQSxFQUFHO0VBQ2JDLE9BQU8sQ0FBQ0QsR0FBRyxDQUFDLE1BQU0sQ0FBQztBQUNyQjtBQUVBQSxHQUFHLENBQUMsQ0FBQztBQUVMLEtBQUtFLENBQUMsR0FBRyxDQUFDLEVBQUVBLENBQUMsR0FBRyxFQUFFLEVBQUVBLENBQUMsRUFBRSxFQUFDO0VBQ3BCRixHQUFHLENBQUMsQ0FBQztBQUNULEMiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9yZXN0YXVyYW50Ly4vc3JjL2luZGV4LmpzIl0sInNvdXJjZXNDb250ZW50IjpbImZ1bmN0aW9uIGxvZygpIHtcbiAgY29uc29sZS5sb2coXCJIZXV5XCIpO1xufVxuXG5sb2coKTtcblxuZm9yIChpID0gMDsgaSA8IDQwOyBpKyspe1xuICAgIGxvZygpXG59Il0sIm5hbWVzIjpbImxvZyIsImNvbnNvbGUiLCJpIl0sInNvdXJjZVJvb3QiOiIifQ==
